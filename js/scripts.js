70c46a3c26554df4c3b9951f0621d42332ad3e24
Abas Dolati <lostangel619@gmail.com>
2023-11-05 23:48:54 +0330

    fix navbar responsive, add usr list

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,32 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
 
-if (document.getElementById('btn-menu') !== null) {
-    var btnMenu = document.getElementById('btn-menu');
-    var sidebar = document.getElementById('sidebar');
-    var pagewrapper = document.getElementById('pageWrapper');
-    btnMenu.onclick = () => {
-        sidebar.classList.toggle('hidden')
-        pagewrapper.classList.toggle('full-width')
-    }
+if (document.getElementById("btn-menu") !== null) {
+  var btnMenu = document.getElementById("btn-menu");
+  var sidebar = document.getElementById("sidebar");
+  var sidebarOverlay = document.querySelector(".side-overlay");
+  var pagewrapper = document.getElementById("pageWrapper");
+  btnMenu.onclick = () => {
+    sidebarOverlay.classList.toggle("active");
+    sidebar.classList.toggle("hidden");
+    pagewrapper.classList.toggle("full-width");
+  };
+  sidebarOverlay.onclick = () => {
+    sidebarOverlay.classList.remove("active");
+    sidebar.classList.remove("hidden");
+    pagewrapper.classList.remove("full-width");
+  };
 }
 
-
 function toggleRadio_files(radioNumber) {
-    const allfilesForm = document.querySelectorAll('.files-form');
-    const activeForm = document.getElementById(`files-${radioNumber}`);
-    allfilesForm.forEach(fileform => {
-        fileform.classList.remove('active')
-    });
-    activeForm.classList.add('active')
+  const allfilesForm = document.querySelectorAll(".files-form");
+  const activeForm = document.getElementById(`files-${radioNumber}`);
+  allfilesForm.forEach((fileform) => {
+    fileform.classList.remove("active");
+  });
+  activeForm.classList.add("active");
 }
