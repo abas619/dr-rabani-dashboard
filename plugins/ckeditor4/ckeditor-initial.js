389642d6cf06d687729f77c341e475393593fba3
Abas Dolati <lostangel619@gmail.com>
2023-12-05 00:11:41 +0330

    + more pages, fix ui, ux

---
+++ plugins/ckeditor4/ckeditor-initial.js
@@ -0,0 +1,13 @@
+CKEDITOR.replace("editor1", {
+  customConfig: "../config.js",
+});
+
+CKEDITOR.config.height = 200;
+CKEDITOR.config.width = "auto";
+CKEDITOR.config.language = "fa";
+
+// CKEDITOR.editorConfig = function (config) {
+//     config.language = 'fa';
+//     config.uiColor = '#F7B42C';
+//     config.toolbarCanCollapse = true;
+// };
