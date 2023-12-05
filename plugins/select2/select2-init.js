32e735034d9318749e243972ee311947ec6ac685
Abas Dolati <lostangel619@gmail.com>
2023-12-05 22:25:03 +0330

    + skills-list, zones,discounts

--- plugins/select2/select2-init.js
+++ plugins/select2/select2-init.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
-    $('.select2-basic-single').select2();
+  $(".select2-basic-single").select2();
+  $(".select2-basic-multiple").select2({
+    //   placeholder: "RTL Select",
+    dir: "rtl",
+  });
 });
